af25e76d9d567a949723d426e7b7fe00409991bc
sithijasanthiya <sithijas322@gmail.com>
2023-10-25 19:43:04 +0530

    doctor card section fetch done

--- src/screens/help.jsx
+++ src/screens/help.jsx
@@ -1,3 +1,51 @@
+/*in the firestore database i have the table called doctors and doctors have properties such as firstName, lastName, designation, availableHrs and imageUrl as properties.
+now i have a doctorCard component and a doctors page in my react native expo application. i need to fetch those doctors information in to the doctor cards and display in the doctors page.
+here are my codes for Doctors page and doctorCard page. Modify these codes for my requirements*/
+
+// doctors page
+import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { DoctorCard, RoundInputField } from '../components';
+import { dimen } from '../../theme';
+const Doctors = () => {
+  const navigation = useNavigation();
+  
+  return (
+    <>
+      <GestureHandlerRootView >
+        <ScrollView style={styles.container}>
+           <RoundInputField
+             placeholder='Search Doctor'
+           />
+          
+            <TouchableOpacity onPress={()=> navigation.navigate('ScheduleAppointment')}>
+            <DoctorCard
+          
+              
+            />
+          </TouchableOpacity>
+          
+        </ScrollView>
+      </GestureHandlerRootView>
+    </>
+  );
+};
+
+export default Doctors;
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: 'white',
+    height: '100%',
+    padding: dimen.default,
+  },
+});
+
+//doctor card page
 import { colors, dimen, typography } from '../../theme';
 import { StyleSheet, Text, View } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
@@ -6,12 +54,12 @@ import { Image } from '@rneui/base';
 import React, { useState } from 'react';
 
 export const DoctorCard = ({ doctor }) => {
+  
+  const [rating, setRating] = useState(doctor.rating);
 
-  // const [rating, setRating] = useState(doctor.rating);
-
-  // const handleStarRatingPress = (newRating) => {
-  //   setRating(newRating);
-  // };
+  const handleStarRatingPress = (newRating) => {
+    setRating(newRating);
+  };
   
   return (
     <View style={styles.container}>
@@ -26,17 +74,20 @@ export const DoctorCard = ({ doctor }) => {
           <Text style={styles.title}>{doctor.name}</Text>
           <View style={styles.statusContainer}>
             <FontAwesome name="circle" size={12} color={colors.successGreen} />
-            {/* <Text style={styles.status}>{doctor.status}</Text> */}
+            <Text style={styles.status}>{doctor.status}</Text>
           </View>
         </View>
         <View style={styles.row}>
           <Text style={styles.specialization}>{doctor.specialization}</Text>
         </View>
         {/* <View style={styles.row}>
-          <StarRatings rating={doctor.rating} />
+          <StarRatings rating={rating} onStarRatingPress={handleStarRatingPress} />
         </View> */}
         <View style={styles.row}>
-          <Text style={styles.time}>{doctor.availableHrs}</Text>
+          <Text style={styles.time}>
+            {doctor.startTime}-{doctor.endTime}
+          </Text>
+          <Text style={styles.fee}>{doctor.fee}</Text>
         </View>
       </View>
     </View>
@@ -105,3 +156,4 @@ const styles = StyleSheet.create({
     color: colors.font,
   },
 });
+
