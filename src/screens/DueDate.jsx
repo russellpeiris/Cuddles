eeb282375894c2641c99e097f7a1070882c93762
IT21258312 <99283261+IT21258312@users.noreply.github.com>
2023-10-26 15:54:44 +0530

    Due Date Calculation Added

--- src/screens/DueDate.jsx
+++ src/screens/DueDate.jsx
@@ -1,16 +1,51 @@
-import { DropDown, MenuButtons, PrimaryButton, RoundInputField } from '../components';
+import { DatePicker, DropDown, InputField, MenuButtons, PrimaryButton, RoundInputField } from '../components';
 import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler'
 import { colors, dimen, typography } from '../../theme';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 import React, { useState } from 'react';
 
 const DueDate = () => {
+  const[inputs, setInputs] = useState({
+    dueDate:'',
+  })
+  const [date, setDate] = useState(new Date());
+  const[estimatedDueDate, setEstimatedDueDate] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
   const[typeOpen, setTypeOpen] = useState(false);
   const[typeValue, setTypeValue] = useState(null);
   const [types, setTypes] = useState([
     {label:'Last Menstrual Period', value:'last menstrual period'},
     {label:'Ultrasound Scan', value:'ultrasound scan'}
   ])
+
+  const toggleDatePicker = () => {
+    setIsVisible(!isVisible);
+  };
+  
+  const handlePicker = (event, selectedDate) => {
+    if (event.type == 'set') {
+      const currentDate = selectedDate;
+      setDate(currentDate);
+
+      if (Platform.OS === 'android') {
+        toggleDatePicker();
+        setInputs({ ...inputs, dueDate: currentDate.toDateString() });
+       // setError((prevError) => ({ ...prevError, dueDate: '' }));
+      }
+    } else {
+      toggleDatePicker();
+    }
+  };
+
+  const calculateDueDate = () => {
+    if (inputs.dueDate) {
+      const selectedDate = new Date(inputs.dueDate);
+      const estimatedDueDate = new Date(selectedDate);
+      estimatedDueDate.setDate(estimatedDueDate.getDate() + 7 * 40); // Adding 40 weeks
+      setEstimatedDueDate(estimatedDueDate);
+    }
+  };
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.formContainer}>
@@ -38,15 +73,43 @@ const DueDate = () => {
           />
         </View>
         <View>
-          <RoundInputField
-            onBlur={() => {}}
-            width="100%"
-            label="Enter Last Menstruation Date"
-            placeholder="Estimated due date"
-          />
+           {isVisible && (
+                <DatePicker mode="date" display="spinner" value={date} onChange={handlePicker} />
+              )}
+                <Pressable
+                  style={{ margin: 0, padding: 0, width: '100%'}}
+                  onPress={toggleDatePicker}
+                >
+                  <RoundInputField
+                    value={inputs.dueDate}
+                    onChangeText={(value) => {
+                      setInputs({ ...inputs, dueDate: value.toDateString() });
+                      setDate(new Date(value));
+                    }}
+                    type={'text'}
+                    onBlur={() => {}}
+                    width="100%"
+                    label="First day of last period"
+                    placeholder="DD MMM YYYY"
+                    editable={false}
+                  />
+                </Pressable>
         </View>
-        <View style={{}}>
-          <PrimaryButton text="Continue" />
+        <Text style={{ fontFamily: typography.semiBold,fontSize:typography.default, color: colors.black,marginTop:16}}>
+             Estimated Due Date
+            </Text>
+         <View style={styles.display}>
+            <Text style={{ fontFamily: typography.bold, fontSize:typography.default, color: colors.primaryPink }}>
+            {estimatedDueDate ? estimatedDueDate.toDateString() : 'Estimated due date will be displayed here'}
+           </Text>
+         </View>
+         <View>
+          <Text style={{ fontFamily: typography.regular, color: colors.descriptionGray }}>
+             Please note that this is an estimated due date. Make sure to check it with your doctor on your next appointment.
+          </Text>
+         </View>
+         <View style={{marginTop:40}}>
+          <PrimaryButton text="Calculate" onPress={calculateDueDate} />
         </View>
       </View>
     </GestureHandlerRootView>
@@ -68,5 +131,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     height: '100%',
+  },
+  display: {
+    borderColor: colors.borderGray,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 20,
+    marginTop:dimen.default,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
